c10e79824dd1423763e34e3a17ae15afb1e96ce7
ShrinaAnahosur <162773960+ShrinaAnahosur@users.noreply.github.com>
2025-03-14 00:18:55 +0530

    Create sample.cpp

---
+++ main/sample.cpp
@@ -0,0 +1,10 @@
+//File: sample.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "This is CC Lab 8! \n";
+  cout << "This is Task 4 \n";
+  return 0;
+}
